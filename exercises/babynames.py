0e18df5ef62e1ef28683160b78285e9fef09fdef
Hugo-Siles <hugos@thinknear.com>
2017-07-28 17:10:25 -0400

    Solve reg expressions exercise

--- exercises/babynames.py
+++ exercises/babynames.py
@@ -34,35 +34,67 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+    file_object = open(filename, 'r')
+    file_content = file_object.read()
+
+    year_string = search_year_in_file(file_content)
+    if not year_string:
+        return 'year not found...'
+
+    result = [year_string]
+    top_names_lines = re.findall('<td>(\d+)</td><td>(\w[^<]*)</td><td>(\w[^<]*)</td>', file_content)
+    for top_name_line in top_names_lines:
+        result.append(top_name_line[1] + ' ' + top_name_line[0])
+        result.append(top_name_line[2] + ' ' + top_name_line[0])
+    return sorted(result)
+
+
+def search_year_in_file(file_content):
+    year_line_match = re.search('Popularity in.*', file_content)
+    if not year_line_match:
+        return None
+
+    year_line = year_line_match.group()
+    year_str_match = re.search('\d\d\d\d', year_line)
+    if not year_str_match:
+        return None
+
+    return year_str_match.group()
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
+    if not args:
+        print 'usage: [--summaryfile] file [file ...]'
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
-  
+    for file_name in args:
+        names_list = extract_names(file_name)
+
+        if not summary:
+            print names_list
+        else:
+            file_object = open(file_name + '.summary', 'w')
+            for line in names_list:
+                file_object.write("%s\n" % line)
+            file_object.close()
+
 if __name__ == '__main__':
-  main()
+    main()
